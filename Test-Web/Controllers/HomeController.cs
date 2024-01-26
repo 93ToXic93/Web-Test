dc643c12cf39459e52c3152e699478eaaa2b9d2e
93ToXic93 <122994130+93ToXic93@users.noreply.github.com>
2024-01-27 01:26:47 +0200

    Today i  made ny db, my create controller and the services with create method! Added About page too.

--- Test-Web/Controllers/HomeController.cs
+++ Test-Web/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace Test_Web.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
